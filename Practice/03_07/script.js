c23d24c83202b37370c696e1f4b24ba7b5e49894
j-hall670 <joe.hall670@gmail.com>
2021-10-05 16:50:15 +0100

    Creating objects practical
    Made some objects with properties and methods. Generally played around with the code. Pretty easy so far so that's nice.

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,44 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+// Object
+const mug = {
+  // Properties
+  volume: 20,
+  handle: true,
+  colour: "White",
+  state: "Empty",
+
+  //Nested Object
+  coffee: {
+    make: "Gold",
+    temperature: 0,
+    colour: "Black",
+    milk: 0,
+    sugar: 0,
+  },
+
+  // Function
+  fill: function () {
+    if (this.state == "Empty") {
+      this.state = "Full";
+      this.coffee.temperature = 100;
+      console.log(
+        "Mug: ",
+        this.state,
+        "and Temperature: ",
+        this.coffee.temperature
+      );
+    } else {
+      console.log("Mug already full");
+    }
+  },
+};
+
+const notebook = {
+  colour: "Black",
+  type: "Ring-bind",
+  numPages: 100,
+  lined: true,
+};
